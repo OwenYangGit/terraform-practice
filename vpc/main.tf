d5eec51e9d62e8dc5d34d7c639a080fe92f0bfa2
Owen <ubuntu@ip-172-31-46-201.ap-northeast-1.compute.internal>
2020-04-23 06:25:53 +0000

    Add basic vpc network private subnet to use nat gateway and allocte a elastic ip address

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,23 +9,53 @@ resource "aws_vpc" "prod" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id     = "${aws_vpc.prod.id}"
   cidr_block = "10.0.10.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = "false"
+  availability_zone = "ap-northeast-1a"
 
   tags = {
-    Name = "ProdPublicSubnet"
+    Name = "ProdPublicSubnet-AZ1"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "public_subnet_2" {
   vpc_id     = "${aws_vpc.prod.id}"
-  cidr_block = "10.0.20.0/24"
+  cidr_block = "10.0.110.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone = "ap-northeast-1c"
 
   tags = {
-    Name = "ProdPrivateSubnet"
+    Name = "ProdPublicSubnet-AZ2"
   }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id     = "${aws_vpc.prod.id}"
+  cidr_block = "10.0.20.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "ProdPrivateSubnet-AZ1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id     = "${aws_vpc.prod.id}"
+  cidr_block = "10.0.120.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "ProdPrivateSubnet-AZ2"
+  }
+}
+resource "aws_eip" "nateip" {
+  vpc      = true
+}
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = "${aws_eip.nateip.id}"
+  subnet_id     = "${aws_subnet.public_subnet_1.id}"
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -44,8 +74,29 @@ resource "aws_route_table" "public_rt" {
     gateway_id = "${aws_internet_gateway.igw.id}"
   }
 }
+resource "aws_route_table" "private_rt" {
+  vpc_id = "${aws_vpc.prod.id}"
 
-resource "aws_route_table_association" "public_rt_binding" {
-  subnet_id      = "${aws_subnet.public_subnet.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.natgw.id}"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_binding_1" {
+  subnet_id      = "${aws_subnet.public_subnet_1.id}"
   route_table_id = "${aws_route_table.public_rt.id}"
+}
+
+resource "aws_route_table_association" "public_rt_binding_2" {
+  subnet_id      = "${aws_subnet.public_subnet_2.id}"
+  route_table_id = "${aws_route_table.public_rt.id}"
+}
+resource "aws_route_table_association" "private_rt_binding_1" {
+  subnet_id      = "${aws_subnet.private_subnet_1.id}"
+  route_table_id = "${aws_route_table.private_rt.id}"
+}
+resource "aws_route_table_association" "private_rt_binding_2" {
+  subnet_id      = "${aws_subnet.private_subnet_2.id}"
+  route_table_id = "${aws_route_table.private_rt.id}"
 }
--- vpc/output.tf
+++ vpc/output.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
   value = "${aws_vpc.prod.id}"
 }
 
-output "igw_id" {
-  value = "${aws_internet_gateway.igw.id}"
+output "vpc_cidr" {
+  value = "${aws_vpc.prod.cidr_block}"
 }
